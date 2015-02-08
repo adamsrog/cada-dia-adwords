55786ce8054e9352d6020a4e28c8ca376795088f
Roger Adams <adamsrog04@gmail.com>
2015-02-08 13:59:41 -0800

    created initial UI with forms and char counts

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	headline: "",
+	line1: "",
+	line2: "",
+	displayUrl: "",
+
+	headlineCharsLeft: Ember.computed('headline', function() {
+		return 25 - this.get('headline').length;
+	}),
+	line1CharsLeft: Ember.computed('line1', function() {
+		return 35 - this.get('line1').length;
+	}),
+	line2CharsLeft: Ember.computed('line2', function() {
+		return 35 - this.get('line2').length;
+	}),
+	urlCharsLeft: Ember.computed('displayUrl', function() {
+		return 35 - this.get('displayUrl').length;
+	}),
+
 });
